a0dc2d8e8904dbbf55ef691035ca9fca5804737f
venkatravi110102 <venkatsam60@gmail.com>
2026-02-23 20:15:57 +0530

    Update Home component with new message and About Us link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom"
+
 export const Home=()=>{
     return(
         <div>
-            this is home page
+            this is home page - new changes
+            <Link to="/about-us">About Us</Link>
         </div>
     )
 }
